1d530ae2fd65715971e220618adb06d38f7a70a6
viv <kamonwun158@gmail.com>
2019-12-08 11:28:44 +0700

    Add message

--- src/lab1/Hello.java
+++ src/lab1/Hello.java
@@ -4,7 +4,7 @@ public class Hello {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello WorId,I am viv");
+        System.out.println("Hello WorId,I am sis");
         System.out.println("MIT");
         System.out.println("Faculty of Management Technology");
         System.out.println("Saiyai");

4ae77ae35eeecb7d1aa30207a754516513b5f9db
Kamonwun <58645809+Kamonwun@users.noreply.github.com>
2019-12-08 11:20:34 +0700

    Update Hello.java

--- src/lab1/Hello.java
+++ src/lab1/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
         System.out.println("Hello WorId,I am viv");
         System.out.println("MIT");
         System.out.println("Faculty of Management Technology");
-
+        System.out.println("Saiyai");
 
 
 

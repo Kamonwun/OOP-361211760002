eb25ba5b91a0ca48d42b136f382247b223622cbb
viv <kamonwun158@gmail.com>
2020-01-15 11:20:08 +0700

    Array2

--- src/lad4/MethodEx.java
+++ src/lad4/MethodEx.java
@@ -1,43 +1,41 @@
-package lad4;
+package Lab4;
 
 public class MethodEx {
 
-    //1.No return No parmater : คือ เมธอดที่ไม่มีการรับค่าพารามิเตอร์ และไม่คืนค่าใดกลับจากนั้น procss แล้ว
-    //2.No return l or more parameter : คือ เมธอดที่มีการรับค่าพารามิเตอร์
-    //3.Return l value, l or more parameter : คือ เมธอดที่มีการรับพารามิเตอร์ตั้งแต่ 1 หรือมากกว่า และ มีการคืนค่ากลับไปส่วรที่เรียกใช้งาน (เรียกใช้จาก
-    // ที่ไหน คืนค่าไปที่นั้น)
+    //1.No return No parameter : คือ เมธอดที่ไม่มีการรับค่าพารามิเตอร์ และไม่คืนค่าใดกลับหลังจาก process แล้ว
+    //2.No return 1 or more parameter : คือ เมธอดที่มีการรับพารามิเตอร์ตั้งแต่ 1 หรือมากกว่า และ ไม่มีค่าใดกลับหลังจาก process แล้ว
+    //3.Return 1 value, 1 or more parameter : คือ ้มธอดที่มีการรับพารามิเตอร์ตั้งแต่ 1 หรือมากกว่า และ มีการคืนค่ากลับไปยังส่วนที่เรียกใช้งาน (เรียกใช้จากที่ไหน คืนค่าไปที่นั่น)
     public static void main(String[] args) {
 
-        System.out.println("Hello from Main ().");
-        //calling method A ()
+        System.out.println("Hello from Main().");
+        //calling method A()
         A();
         A();
         A();
-        //calling method B ()
-        B("MIT","RUST");
-        //calling method c ()
-        //1.ประกาศตัวแปรชนิดเดียวกันมาเก็บค่ามราเมธอดส่งกลับมาให้
-        int summation = C (10,20);
-        System.out.println("The summation of x and y:"+summation);
-        //2.เรียกใช้
-        System.out.println("The summation of x and y (2):"+C(50,50,));
+        //calling method B()
+        B("MIT","RUTS");
+        //calling method C()
+        //1.ประกาศด้วยตัวแปรชนิดเดียวกันมาเก็บค่าที่เมธอดส่งกลับมาให้
+        int summation = C(10,20);
+        System.out.println("The summation of x and y : "+summation);
+        //2.เรียกใช้เมธอดโดยไม่ประกาศตัวแปรใดๆ มาเก็บค่าที่ส่งมา
+        System.out.println("The summation of x and y (2): "+C(50,50));
     }//main
 
-    public static void A () {
-        System.out.println("Hello from A().");
+    public static void A(){
+        System.out.println("Hello from A ().");
     }//A
-    public  static void B(String msg, String msg2){
+    public static void B(String msg, String msg2){
         System.out.println("Hello from B().");
         System.out.println("Message: "+msg);
-        System.out.println("Message2:+msg2");
+        System.out.println("Message2: "+msg2);
 
     }//B
     public static int C(int x, int y){
-        System.out.println("Hello from C ().");
+        System.out.println("Hello from C().");
         int sum = x + y;
         return sum;
 
-    }
+    }//C
+
 }//class
-
-
--- src/lad4/Method_Exercise.java
+++ src/lad4/Method_Exercise.java
@@ -1,34 +1,35 @@
-package lad4;
+package Lab4;
 
 import java.util.Scanner;
 
 public class Method_Exercise {
-    //1.เขียนโปรแกรมเพื่อรับค่าจำนวนจริง 2 จำนวน จากนั้นสร้างเมธอดสำหรับ บวก คูณ ลบ หาร แสดงทางจอภาพ
-    public static void main (String [] args) {
+    //1.เขียนโปรแกรมเพื่อนรับค่าจำนวนจริง 2 จำนวน จากนั้นสร้างเมธอดสำหรับ บวก ลบ คูณ หาร แสดงผลทางจอภาพ
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter number 1: ");
+        System.out.println("Enter a number 1: ");
         double x = sc.nextDouble();
-        System.out.println("Enter number 2: ");
+        System.out.println("Enter a number 2: ");
         double y = sc.nextDouble();
 
         //display
-        System.out.println("The summation of"+x+"and"+y+": "+sum(x,y));
-        System.out.println("The summation of"+x+"and"+y+": "+sum(x,y));
-        System.out.println("The summation of"+x+"and"+y+": "+sum(x,y));
-        System.out.println("The summation of"+x+"and"+y+": "+sum(x,y));
-    } //main
-     public static  double sum(double x, double y){
+        System.out.println("The summation of "+x+"and"+y+": "+sum(x,y));
+        System.out.println("The summation of "+x+"and"+y+": "+sub(x,y));
+        System.out.println("The summation of "+x+"and"+y+": "+mul(x,y));
+        System.out.println("The summation of "+x+"and"+y+": "+div(x,y));
+    }//main
+    public static double sum (double x, double y){
 
         return x + y;
     }//บวก
-    public static  double sub(double x, double y){
+    public static double sub(double x, double y){
 
         return x - y;
     }//ลบ
-    public static  double mul(double x, double y){
+    public static double mul(double x, double y){
 
         return x * y;
     }//คูณ
-
+    public static double div (double x,double y){
+        return x/y;
+    }
 }
-
